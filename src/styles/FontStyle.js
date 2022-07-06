39a131f201b5797db699dfe403ff8667753c995d
liangwu <liangwucas@gmail.com>
2022-07-06 17:43:25 +0800

    update font styles

--- src/styles/FontStyle.js
+++ src/styles/FontStyle.js
@@ -1,10 +1,8 @@
 export default ()=>{
   return(`
-
     .App *, .App div, .App div * {
-      font-family: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+      font-family: system-ui,-apple-system,BlinkMacSystemFont,segoe ui,Roboto,Helvetica,Arial,sans-serif,apple color emoji,segoe ui emoji,segoe ui symbol;
     }
-
     .FontBold {
       font-weight: bold;
     }
